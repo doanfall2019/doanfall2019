7d1d9bad7017917221cf62887ec7375ea7acf5f7
tunm123 <tunmps07784@fpt.edu.vn>
2019-10-23 19:47:26 +0700

    23/10/2019 19/47 danh gia chi tiet

--- app/src/main/java/com/example/myapplication/View/TrangChu/TrangChuActivity.java
+++ app/src/main/java/com/example/myapplication/View/TrangChu/TrangChuActivity.java
@@ -44,12 +44,12 @@ public class TrangChuActivity extends AppCompatActivity implements ViewXuLyMenu,
 //    public static final String SERVER_NAME = "http://192.168.3.6/webservice/apiserver.php";
 //    public static final String SERVER = "http://192.168.3.6/webservice";
 
-//    public static final String SERVER_NAME = "http://10.22.211.192/webservice/apiserver.php";
-//    public static final String SERVER = "http://10.22.211.192/webservice";
+    public static final String SERVER_NAME = "http://10.22.211.243/webservice/apiserver.php";
+    public static final String SERVER = "http://10.22.211.243/webservice";
 //
 // truong
-public static final String SERVER_NAME = "http://10.88.54.71/webservice/apiserver.php";
- public static final String SERVER = "http://10.88.54.71/webservice";
+//public static final String SERVER_NAME = "http://10.88.54.71/webservice/apiserver.php";
+// public static final String SERVER = "http://10.88.54.71/webservice";
 
     Toolbar toolbar;
     TabLayout tabLayout;
